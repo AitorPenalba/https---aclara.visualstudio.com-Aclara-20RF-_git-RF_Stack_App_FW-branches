298be81f956c0a4901236f1d71865aa6278f72b4
Siva S <schellappan@hubbell.com>
2022-07-01 15:10:10 +0530

    EP v3.0.81: Os_Task_Summary added for freeRTOS - tasksummary command enabled. - OS_TASK_SummaryfreeRtos function added in Task.c. - This function will print the state of tasks.while using the free RTOS.

--- MTU/Common/Task.c
+++ MTU/Common/Task.c
@@ -1437,6 +1437,68 @@ void OS_TASK_Summary ( bool safePrint )
 #if ( RTOS_SELECTION == FREE_RTOS ) /* FREE_RTOS */
 /***********************************************************************************************************************
  *
+ * Function Name: OS_TASK_SummaryfreeRtos
+ *
+ * Purpose: This function will print the state of tasks.while using the free RTOS
+ *
+ * Arguments: None
+ *
+ * Returns: None
+ *
+ * Notes:
+ *
+ **********************************************************************************************************************/
+
+void OS_TASK_SummaryfreeRtos(void)
+{
+   int8_t index = 0;
+   char taskState[6][10]={"Running","Ready","Blocked","Suspended","Deleted","Invalid" };
+   char taskInformation[9][25]={"TaskName","TaskNumber","TaskCurrentPriority","TaskBasePriority","TaskState","TaskRunTimeCounter","TaskStackDepth","TaskStackHighWaterMark","TaskStackBase"};
+   char taskInfo[9][25]={"TName","TNumber","TCP","TBP","TState","TRTC","TSD","TSHWM","TSB"};
+   char buffer[150];
+   TaskStatus_t taskStatusInfo;
+   TaskHandle_t taskHandle;
+   OS_TASK_Template_t const *pTaskList;
+
+   DBG_logPrintf('R',"OS_TASK_SUMMARY");
+   while(index<9)
+   {
+      DBG_logPrintf('R',"%20s : %s",taskInfo[index],taskInformation[index] );
+      index++;
+   }
+   snprintf(buffer,150,"%10s %10s %10s %10s %10s %10s %10s %10s %10s\r\n",
+                  taskInfo[0],
+                  taskInfo[1],
+                  taskInfo[2],
+                  taskInfo[3],
+                  taskInfo[4],
+                  taskInfo[5],
+                  taskInfo[6],
+                  taskInfo[7],
+                  taskInfo[8]);
+   DBG_logPrintf('R',"%s",buffer );
+   for (pTaskList = &Task_template_list[0]; 0 != pTaskList->TASK_TEMPLATE_INDEX; pTaskList++)
+   {
+      taskHandle = xTaskGetHandle((char*)pTaskList->pcName);
+      vTaskGetInfo(taskHandle, &taskStatusInfo, pdTRUE, eInvalid );
+      snprintf(buffer,150,"%10s %10d %10d %10d %10s %10d %10d %10d   0x%x\r\n",
+               (char*)pTaskList->pcName,
+               taskStatusInfo.xTaskNumber,
+               taskStatusInfo.uxCurrentPriority,
+               taskStatusInfo.uxBasePriority,
+               (char*)taskState[taskStatusInfo.eCurrentState],
+               taskStatusInfo.ulRunTimeCounter,
+               pTaskList->usStackDepth,
+               taskStatusInfo.usStackHighWaterMark,
+               taskStatusInfo.pxStackBase);
+      DBG_logPrintf('R',"%s",buffer );
+   }
+}
+#endif
+
+#if ( RTOS_SELECTION == FREE_RTOS ) /* FREE_RTOS */
+/***********************************************************************************************************************
+ *
  * Function Name: OS_TASK_Create_STRT
  *
  * Purpose: This function will Create the Startup Task in FreeRTOS
