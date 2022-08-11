f726b102676fb9a9ae1f99236f62b92ad3a73f44
William Powell <wvpowell@indesign-llc.com>
2022-08-11 18:04:36 -0400

    Added IWDT to BSP, changed programming file from .hex to .bin, added 0xFF fill character up to 0x4000, excluded option settings

--- MTU/Bootloader/R7FA6E1/IAR/ra_gen/hal_data.h
+++ MTU/Bootloader/R7FA6E1/IAR/ra_gen/hal_data.h
@@ -4,7 +4,20 @@
 #include <stdint.h>
 #include "bsp_api.h"
 #include "common_data.h"
+#include "r_iwdt.h"
+            #include "r_wdt_api.h"
 FSP_HEADER
+/** WDT on IWDT Instance. */
+extern const wdt_instance_t g_wdt0;
+
+
+/** Access the IWDT instance using these structures when calling API functions directly (::p_api is not used). */
+extern iwdt_instance_ctrl_t g_wdt0_ctrl;
+extern const wdt_cfg_t g_wdt0_cfg;
+
+#ifndef NULL
+void NULL(wdt_callback_args_t * p_args);
+#endif
 void hal_entry(void);
 void g_hal_init(void);
 FSP_FOOTER
