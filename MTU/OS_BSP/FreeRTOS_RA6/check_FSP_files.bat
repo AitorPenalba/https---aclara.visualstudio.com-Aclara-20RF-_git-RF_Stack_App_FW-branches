24f39961f5c5f39e43efa4eaa5197a3efc9b3de3
William Powell <wvpowell@indesign-llc.com>
2022-08-30 13:43:33 -0400

    Update check file scripts to select either BeyondCompare (1st) or WinMerge (2nd) as the visual comparison tool for team compatibility

--- MTU/OS_BSP/FreeRTOS_RA6/check_FSP_files.bat
+++ MTU/OS_BSP/FreeRTOS_RA6/check_FSP_files.bat
@@ -1,12 +1,26 @@
 :: Check for differences between the RASC created FSP folders and the locked-in bootloader folders
-:: Version 1.0
 @echo off
-echo Check for RA6E1 Bootloader FSP File Changes, Version 1.0, 8/17/22
+echo Check for RA6E1 Bootloader FSP File Changes, Version 1.1, 8/30/22
 
-:: Add winmerge path here
-set WINMERGE="C:\Program Files\WinMerge\WinMergeU.exe"
+:: set Winmerge install location
+set WINMERGE_PATH="C:\Program Files\WinMerge\WinMergeU.exe"
 
-:: start winmerge
-%WINMERGE% /r %cd% %cd%\IAR
+:: set Beyond Compare install location
+set BEYOND_COMPARE_PATH="C:\Program Files\Beyond Compare 4\BComp.com"
+
+if exist %BEYOND_COMPARE_PATH% (
+	set MERGE=%BEYOND_COMPARE_PATH%
+) else (
+	if exist %WINMERGE_PATH% (
+		set MERGE=%WINMERGE_PATH%
+	) else (
+		echo Error: requires either %BEYOND_COMPARE_PATH% or %WINMERGE_PATH% - exiting
+		exit /b 0
+	)
+)
+echo Using %MERGE% for comparison
+
+:: start visual merge
+%MERGE% /r %cd% %cd%\IAR
 
 echo Done!
