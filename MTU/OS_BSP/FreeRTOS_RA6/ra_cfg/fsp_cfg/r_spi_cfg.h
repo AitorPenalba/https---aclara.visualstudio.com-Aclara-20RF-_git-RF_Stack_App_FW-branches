e5fcc56801735351c1a7e18696b1240cd0bb65be
Dhruv Gaonkar <dgaonkar@aclaratech.com>
2022-10-21 10:45:15 -0500

    EP v3.0.167: Bug Fixes - Fix the issue that was caused in the last build - Disable DTC on the radio SPI which is unused till now - Remove unused OSPI files from the BSP_FreeRTOS project - Remove test print outs - Non-functional changes

--- MTU/OS_BSP/FreeRTOS_RA6/ra_cfg/fsp_cfg/r_spi_cfg.h
+++ MTU/OS_BSP/FreeRTOS_RA6/ra_cfg/fsp_cfg/r_spi_cfg.h
@@ -2,6 +2,6 @@
 #ifndef R_SPI_CFG_H_
 #define R_SPI_CFG_H_
 #define SPI_CFG_PARAM_CHECKING_ENABLE (BSP_CFG_PARAM_CHECKING_ENABLE)
-                 #define SPI_DTC_SUPPORT_ENABLE (1)
+                 #define SPI_DTC_SUPPORT_ENABLE (0)
                  #define SPI_TRANSMIT_FROM_RXI_ISR (0)
 #endif /* R_SPI_CFG_H_ */
